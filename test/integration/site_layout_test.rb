d24f87b0269ff7ecdc67a6dbd0d55afd625a9f43
Justin Hsieh <kyhsieh@ubnt.com>
2015-03-06 18:50:19 +0800

    finish layout and routes

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout_links" do
+    get root_path
+    assert_template 'static_pages/home'   # test if rendered using the correct view
+    assert_select "a[href=?]", root_path, count: 2   # test if there are 2 links to root path (the "?" is replaced with root_path by Rails)
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+
+  end
+end
