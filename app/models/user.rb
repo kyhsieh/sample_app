cf2bf4c06cbcd38dfe3574eb2d710536fd0251a2
Justin Hsieh <kyhsieh@ubnt.com>
2015-03-23 17:09:31 +0800

    finish login/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+  attr_accessor :remember_token
   # downcase all email address before saving to DB
   before_save { self.email.downcase! }
 
@@ -15,9 +17,30 @@ class User < ActiveRecord::Base
   # add the ability to store HASH version password in DB, must have a DB column with name "password_digest"
   has_secure_password
 
+   #returns the hash digest of the given string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # returns a random token
+  def User.new_token()
+    SecureRandom.urlsafe_base64
+  end
+
+  # remember a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
